878d25af02527a93d6184a52fb9ef1568cb0cac0
Huu Bao Nguyen <huubao2309@gmail.com>
2021-07-04 00:40:26 +0700

    add back button

--- lib/module/scan_qrcode.dart
+++ lib/module/scan_qrcode.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class ScanQrCodePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(title: Text('ScanQrCode'), backgroundColor: Colors.green),
+      body: Container(
+        color: Colors.blue,
+      ),
     );
   }
 }
